8f34016651d691cc94a9c4b694ee6c6e49eecd72
vadla071 <112850456+vadla071@users.noreply.github.com>
2022-10-10 13:52:34 +0530

    Create user ac.cpp

---
+++ user ac.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+int main()
+{
+	char* s;
+	char
+	cout<<"enter the user name";
+		cin>>s;
+	cout<<"re-enter the user name";
+		cin>>a;
+	if(a==s)
+	{
+		cout<<"accepted";
+	}
+	else
+	{
+		cout<<" not accepted";
+	}
+}
